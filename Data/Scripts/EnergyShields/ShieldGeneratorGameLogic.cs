4d604347d70ae8d85edc51d0ac5c99a02e5cc4d8
douxt <douxt@yeah.net>
2017-11-30 21:43:48 +0800

    黑店不生产小凡币 改为money maker生产，个人低保福box

--- Data/Scripts/EnergyShields/ShieldGeneratorGameLogic.cs
+++ Data/Scripts/EnergyShields/ShieldGeneratorGameLogic.cs
@@ -26,9 +26,11 @@ using VRage.Game;
 using VRage.Game.ModAPI;
 using VRage.Game.ObjectBuilders.Definitions;
 using VRage.Utils;
+using VRage.Game.Entity;
 using VRage.Game.Entity.UseObject;
 using Sandbox.Game.Entities.Character;
 using Sandbox.Game.Gui;
+using SpaceEngineers.Game.ModAPI;
 
 namespace Cython.EnergyShields
 {
@@ -42,13 +44,15 @@ namespace Cython.EnergyShields
 
         Sandbox.ModAPI.IMyTerminalBlock terminalBlock;
 
+		private static ulong frameShift = 0;
+        private ulong Frame;
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
             m_generator = Entity as Sandbox.ModAPI.IMyAssembler;
             m_parent = Entity as IMyCubeBlock;
             builder = objectBuilder;
 
-            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME;
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME; //|= MyEntityUpdateEnum.EACH_FRAME
 
             terminalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
         }
@@ -59,12 +63,18 @@ namespace Cython.EnergyShields
 
             if (m_generator.IsWorking)
             {
-				IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+                Frame = frameShift++;
+                if (Frame % 30 != 0)
+                {
+                    return;
+                }
+
+                IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
 
                 if (!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
                 {
-                    inventory.AddItems(5, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
-                    terminalBlock.RefreshCustomInfo();
+                    //inventory.AddItems(5, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                    //terminalBlock.RefreshCustomInfo();
                 }
 				IMyInventory inventory1 = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(1) as IMyInventory;
 				if (!inventory1.ContainItems(10, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" }))
@@ -72,6 +82,29 @@ namespace Cython.EnergyShields
                     inventory1.AddItems(1, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" });
                     terminalBlock.RefreshCustomInfo();
                 }
+
+
+                //List<IMyPlayer> players = new List<IMyPlayer>();
+                //MyAPIGateway.Players.GetPlayers(players, x => x.Controller != null && x.Controller.ControlledEntity != null);
+                //foreach (IMyPlayer player in players)
+                //{
+                //    if (player.Controller.ControlledEntity is IMyCharacter)
+                //    {
+
+                //        MyEntity entity = player.Controller.ControlledEntity.Entity as MyEntity;
+                //        if (entity.HasInventory)
+                //        {
+                //            inventory = entity.GetInventoryBase() as MyInventory;
+                //            if (!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
+                //            {
+                //                inventory.AddItems(5, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                //                terminalBlock.RefreshCustomInfo();
+                //            }
+                //        }
+                //    }
+
+                //}
+
 
             }
         }
@@ -83,5 +116,221 @@ namespace Cython.EnergyShields
 
     }
 
-	}
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Assembler), true, "MoneyMaker")]
+    public class MoneyMaker : MyGameLogicComponent
+    {
+        private MyObjectBuilder_EntityBase builder;
+        private Sandbox.ModAPI.IMyAssembler m_generator;
+        private IMyCubeBlock m_parent;
+
+        Sandbox.ModAPI.IMyTerminalBlock terminalBlock;
+
+        private static ulong frameShift = 0;
+        private ulong Frame;
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            m_generator = Entity as Sandbox.ModAPI.IMyAssembler;
+            m_parent = Entity as IMyCubeBlock;
+            builder = objectBuilder;
+
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME; //|= MyEntityUpdateEnum.EACH_FRAME
+
+            terminalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
+        }
+        #region UpdateBeforeSimulation
+        public override void UpdateBeforeSimulation100()
+        {
+            base.UpdateBeforeSimulation100();
+
+            if (m_generator.IsWorking)
+            {
+                Frame = frameShift++;
+                if (Frame % 30 != 0)
+                {
+                    return;
+                }
+
+                IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+
+                if (!inventory.ContainItems(20000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
+                {
+                    inventory.AddItems(300, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                    terminalBlock.RefreshCustomInfo();
+                }
+                //IMyInventory inventory1 = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(1) as IMyInventory;
+                //if (!inventory1.ContainItems(10, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" }))
+                //{
+                //    inventory1.AddItems(1, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" });
+                //    terminalBlock.RefreshCustomInfo();
+                //}
+
+
+                //List<IMyPlayer> players = new List<IMyPlayer>();
+                //MyAPIGateway.Players.GetPlayers(players, x => x.Controller != null && x.Controller.ControlledEntity != null);
+                //foreach (IMyPlayer player in players)
+                //{
+                //    if (player.Controller.ControlledEntity is IMyCharacter)
+                //    {
+
+                //        MyEntity entity = player.Controller.ControlledEntity.Entity as MyEntity;
+                //        if (entity.HasInventory)
+                //        {
+                //            inventory = entity.GetInventoryBase() as MyInventory;
+                //            if (!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
+                //            {
+                //                inventory.AddItems(5, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                //                terminalBlock.RefreshCustomInfo();
+                //            }
+                //        }
+                //    }
+
+                //}
+
+
+            }
+        }
+        #endregion
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return builder;
+        }
+
+    }
+
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_CargoContainer), true, "FuBox")]
+    public class FuBox : MyGameLogicComponent
+    {
+        private MyObjectBuilder_EntityBase builder;
+        private Sandbox.ModAPI.IMyCargoContainer m_generator;
+        private IMyCubeBlock m_parent;
+
+        Sandbox.ModAPI.IMyTerminalBlock terminalBlock;
+
+        private static ulong frameShift = 0;
+        private ulong Frame;
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            m_generator = Entity as Sandbox.ModAPI.IMyCargoContainer;
+            m_parent = Entity as IMyCubeBlock;
+            builder = objectBuilder;
+
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME; //|= MyEntityUpdateEnum.EACH_FRAME
+
+            terminalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
+        }
+        #region UpdateBeforeSimulation
+        public override void UpdateBeforeSimulation100()
+        {
+            base.UpdateBeforeSimulation100();
+
+            if (m_generator.IsWorking)
+            {
+                Frame = frameShift++;
+                if (Frame % 30 != 0)
+                {
+                    return;
+                }
+
+                //IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+
+                //if (!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
+                //{
+                //    inventory.AddItems(5, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                //    terminalBlock.RefreshCustomInfo();
+                //}
+                //IMyInventory inventory1 = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(1) as IMyInventory;
+                //if (!inventory1.ContainItems(10, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" }))
+                //{
+                //    inventory1.AddItems(1, new MyObjectBuilder_AmmoMagazine { SubtypeName = "NATO_25x184mm" });
+                //    terminalBlock.RefreshCustomInfo();
+                //}
+
+
+                List<IMyPlayer> players = new List<IMyPlayer>();
+                MyAPIGateway.Players.GetPlayers(players, x => x.Controller != null && x.Controller.ControlledEntity != null);
+                foreach (IMyPlayer player in players)
+                {
+                    if (player.Controller.ControlledEntity is IMyCharacter)
+                    {
+
+                        MyEntity entity = player.Controller.ControlledEntity.Entity as MyEntity;
+                        if (entity.HasInventory)
+                        {
+                            IMyInventory inventory = entity.GetInventoryBase() as MyInventory;
+                            if (!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" }))
+                            {
+                                inventory.AddItems(60, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+                                terminalBlock.RefreshCustomInfo();
+                            }
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+        #endregion
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return builder;
+        }
+
+    }
+
+    //   [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Character),false,"Default_Astronaut")]
+    //  public class SurvivalPlayer : MyGameLogicComponent
+    //  {
+    //      MyObjectBuilder_EntityBase m_objectBuilder = null;
+    //      IMyCharacter player;
+    //      MyInventory inventory;
+    //      MyObjectBuilder_Component componentBuilder;
+    //      string componentToAdd = "SteelPlate";
+
+    //      //if you suscribed to events, please always unsuscribe them in close method 
+    //      public override void Close()
+    //      {
+    //      }
+
+
+    //private static ulong frameShift = 0;
+    //      private ulong Frame;
+
+    //      public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+    //      {
+
+    //          Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+    //          player = Entity as IMyCharacter;
+    //          MyEntity entity = Entity as MyEntity;
+    //          if(entity.HasInventory)
+    //          {
+    //              inventory = entity.GetInventoryBase() as MyInventory;
+    //          }
+    //          componentBuilder = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_Component>(componentToAdd);
+    //      }
+
+    //      public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+    //      {
+    //          return m_objectBuilder;
+    //      }
+
+    //      public override void UpdateBeforeSimulation100()
+    //      {
+    //	//base.UpdateBeforeSimulation100();
+    //	Frame = frameShift++;
+    //          //MyPhysicalInventoryItem? existingComponent = inventory.FindItem(componentBuilder.GetId());
+    //          if (Frame % 30 != 0) {
+    //		return;
+    //	}
+    //	if(!inventory.ContainItems(10000, new MyObjectBuilder_Ingot { SubtypeName = "Coin" })){
+    //		//inventory.AddItems(100, new MyObjectBuilder_Ingot { SubtypeName = "Coin" });
+    //	}
+
+
+
+
+    //      }
+    //  }
+
+}
 
